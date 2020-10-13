f6cc88eb90ce3c695f68caa30ed4cb203ac6ac7f
Christopher Medway <christophermedway@gmail.com>
2020-10-13 14:39:35 +0100

    dragen post cron wren modifications

--- dragen_post_cron.sh
+++ dragen_post_cron.sh
@@ -4,95 +4,74 @@ set -euo pipefail
 
 # Cron job to find dragen results and perform any post processing required.
 
-
-dragen_results_dir=/share/data/results/dragen_results/
-dragen_temp_dir=/share/data/results/dragen_temp/
+dragen_results_dir=/Output/results/
 
 # loop through each folder and find runs which have finished the dragen side of stuff
 
 
-
-
-
-for path in $(find /data/results/dragen_results/ -maxdepth 3 -mindepth 3 -type f -name "dragen_complete.txt" -exec dirname '{}' \;); do
+for path in $(find $dragen_results_dir -maxdepth 3 -mindepth 3 -type f -name "dragen_complete.txt" -exec dirname '{}' \;); do
 
   # for each of those runs find the post processing pipeline we need
   echo $path
 
-  if [ ! -f "$path"/post_processing_started.txt ]; then
+  if [ ! -f "$path"/post_processing_started.txt ] && [ -f "$path"/post_processing_required.txt ]; then
 
-  touch "$path"/post_processing_started.txt
+     touch "$path"/post_processing_started.txt
  
+     runid=$(basename $(dirname "$path"))
+     panel=$(basename "$path")
+     echo $runid
+     echo $panel
 
-
-  runid=$(basename $(dirname "$path"))
-  panel=$(basename "$path")
-  echo $runid
-  echo $panel
-
-  # load the variables file which says which pipeline we need
+     # load the variables file which says which pipeline we need
   
-  if [ ! -f "$path"/"$panel".variables ]; then
-    echo "variables does not exist"
-    exit 0
-  fi
+     if [ ! -f "$path"/"$panel".variables ]; then
+        echo "variables does not exist"
+        exit 0
+     fi
 
 
-  . "$path"/"$panel".variables
+     . "$path"/"$panel".variables
 
-  echo $post_processing_pipeline
-  echo $post_processing_pipeline_version
+     echo $post_processing_pipeline
+     echo $post_processing_pipeline_version
 
 
-  mkdir -p "$dragen_temp_dir"/"$runid"/"$panel"/results
+     mkdir -p "$path"/post_processing
 
-  set +u
-  source /home/transfer/miniconda3/bin/activate $post_processing_pipeline
-  set -u
+     cd "$path"/post_processing
+
+     set +u
+     source activate $post_processing_pipeline
+     set -u
   
-  # copy pipeline scripts
-  cp /data/diagnostics/pipelines/"$post_processing_pipeline"/"$post_processing_pipeline"-"$post_processing_pipeline_version"/config/"$panel"/"$panel"_pbs.config \
-  "$dragen_temp_dir"/"$runid"/"$panel"/
+     # copy pipeline scripts
+     cp /data/diagnostics/pipelines/"$post_processing_pipeline"/"$post_processing_pipeline"-"$post_processing_pipeline_version"/config/"$panel"/"$panel"_wren.config .
+     
+     cp -r /data/diagnostics/pipelines/"$post_processing_pipeline"/"$post_processing_pipeline"-"$post_processing_pipeline_version"/config .
 
-  cp /data/diagnostics/pipelines/"$post_processing_pipeline"/"$post_processing_pipeline"-"$post_processing_pipeline_version"/"$post_processing_pipeline".nf \
-  "$dragen_temp_dir"/"$runid"/"$panel"/
+     cp /data/diagnostics/pipelines/"$post_processing_pipeline"/"$post_processing_pipeline"-"$post_processing_pipeline_version"/"$post_processing_pipeline".nf .
 
-  cp -r /data/diagnostics/pipelines/"$post_processing_pipeline"/"$post_processing_pipeline"-"$post_processing_pipeline_version"/bin "$dragen_temp_dir"/"$runid"/"$panel"/
-
-
-  cd "$dragen_temp_dir"/"$runid"/"$panel"/
-  # run nextflow
-  nextflow -C \
-  "$dragen_temp_dir"/"$runid"/"$panel"/"$panel"_pbs.config \
-  run \
-  -E \
-  "$dragen_temp_dir"/"$runid"/"$panel"/"$post_processing_pipeline".nf \
-  --bams "$dragen_results_dir"/"$runid"/"$panel"/\*/\*\{.bam,.bam.bai\} \
-  --worklist_id "$runid" \
-  --vcf "$dragen_results_dir"/"$runid"/"$panel"/"$runid"\{.vcf.gz,.vcf.gz.tbi\} \
-  --variables "$dragen_results_dir"/"$runid"/"$panel"/\*/\*.variables \
-  --publish_dir "$dragen_temp_dir"/"$runid"/"$panel"/results \
-  --sequencing_run "$runid" \
-  -work-dir "$dragen_temp_dir"/"$runid"/"$panel"/work &> "$dragen_temp_dir"/"$runid"/"$panel"/results/pipeline.log 
-
-  touch "$dragen_temp_dir"/"$runid"/"$panel"/results/post_processing_finished.txt
-  
-  # move results and pipeline files
-
-  mv "$dragen_temp_dir"/"$runid"/"$panel"/results "$dragen_results_dir"/"$runid"/"$panel"/
-  mv "$dragen_temp_dir"/"$runid"/"$panel"/*.nf "$dragen_results_dir"/"$runid"/"$panel"/
-  mv "$dragen_temp_dir"/"$runid"/"$panel"/*.config "$dragen_results_dir"/"$runid"/"$panel"/
-  mv "$dragen_temp_dir"/"$runid"/"$panel"/bin "$dragen_results_dir"/"$runid"/"$panel"/results
+     cp -r /data/diagnostics/pipelines/"$post_processing_pipeline"/"$post_processing_pipeline"-"$post_processing_pipeline_version"/bin .
 
 
-  # delete work dir
-  rm -r "$dragen_temp_dir"/"$runid"/"$panel"/work 
+     # run nextflow
+     nextflow -C \
+     "$panel"_wren.config \
+     run \
+     -E \
+     "$post_processing_pipeline".nf \
+     --bams ../\*/\*\{.bam,.bam.bai\} \
+     --vcf ../"$runid"\{.vcf.gz,.vcf.gz.tbi\} \
+     --variables ../\*/\*.variables \
+     --publish_dir results \
+     --sequencing_run "$runid" \
+     -work-dir work &> pipeline.log 
 
-  fi
+     # delete work dir
+     rm -r work     
 
-
-
-
+     fi
 
 done
 
